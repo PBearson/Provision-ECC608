c7998c2f9974ef877bf13337d8a98d4f722cf6d0
PBearson <peabryan95@gmail.com>
2021-03-30 17:04:29 -0400

    Major updates

--- main/main.c
+++ main/main.c
@@ -9,64 +9,17 @@
 #include <stdio.h>
 #include "cryptoauthlib.h"
 #include "atcacert/atcacert_client.h"
-#include "atcacert/atcacert_host_hw.h"
-#include "driver/i2c.h"
-#include "crypto_settings.h"
+// #include "atcacert/atcacert_host_hw.h"
+// #include "driver/i2c.h"
+#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "csr_def.h"
+#include "esp_log.h"
 
 void print_csr(char*, size_t);
 void print_pubkey(uint8_t*, size_t);
 
-/*
- * Check the return status of a function.
- * Assume 0 = SUCCESS.
- * If non-zero, stop the code.
- */
-void check_ret(int ret)
-{
-	if(ret != 0)
-	{
-		printf("There is a problem. Error code %X. Refer here for more information:\n%s\n", ret, "https://github.com/MicrochipTech/cryptoauthlib/blob/master/lib/atca_status.h");
-		while(true) vTaskDelay(1000 / portTICK_PERIOD_MS);
-	}
-}
-
-/*
- * Similar to check_ret except we print the CSR before stalling.
- * Call this if the device fails to generate a public key in slot 0.
- */
-void check_ret_csr(int ret)
-{
-	uint8_t pubkey[64];
-	char csr[512];
-	size_t pubkey_size = 64;
-	size_t csr_size = 512;
-
-	if(ret != 0)
-	{
-		// Using the public key, create a CSR
-		check_ret(atcab_get_pubkey(0, pubkey));
-		check_ret(atcacert_create_csr_pem(&csr_def, csr, &csr_size));
-
-		// Print public key and CSR
-		print_pubkey(pubkey, pubkey_size);
-		print_csr(csr, csr_size);
-		
-		// Still need to check the ret value
-		check_ret(ret);
-	}
-}
-
-/*
- * Lock the ECCx08 data zone.
- */
-void lock_data_zone()
-{
-	check_ret(atcab_lock_data_slot(0));
-	check_ret(atcab_lock_data_zone());
-	printf("Your cryptochip's data zone has been successfully locked.\n");
-}
+static const char *TAG = "provision-ecc608";
 
 /*
  * Print a CSR
@@ -94,39 +47,58 @@ void print_pubkey(uint8_t* pubkey, size_t pubkey_size)
 	printf("\n\n");
 }
 
-/*
- * Create a new private ECC in slot 0 and generate a CSR.
- * Only run this after successfully calling "atcab_init()"
- */
-void run()
-{	
-     	uint8_t pubkey[64];
-	char csr[512];
-	size_t pubkey_size = 64;
-	size_t csr_size = 512;
-	
-	// 1: Generate a private key
-	check_ret_csr(atcab_genkey(0, pubkey));
-	
-	// 2: Create a CSR from the private key
-	check_ret(atcacert_create_csr_pem(&csr_def, csr, &csr_size));
 
-	// Print public key and CSR
-	print_pubkey(pubkey, pubkey_size);
-	print_csr(csr, csr_size);
+// Initialize ECC608
+void initialize_ecc608()
+{
+    int ret;
+    ESP_LOGI(TAG, "Initializing ECC608");
 
-	// Lock data zone
-	lock_data_zone();
+    #ifdef CONFIG_SCAN_FOR_I2C_ADDRESS
+
+    ESP_LOGI(TAG, "Scanning for the correct I2C address");
+    for(int i = 0; i < 256; i++)
+    {
+        ESP_LOGI(TAG, "Scanning address %02X", i);
+        cfg_ateccx08a_i2c_default.atcai2c.slave_address = i;
+        ret = atcab_init(&cfg_ateccx08a_i2c_default);
+        if(!ret) break;
+    }
+
+    #else
+
+    int default_address = cfg_ateccx08a_i2c_default.atcai2c.slave_address;
+    ESP_LOGI(TAG, "Scanning default address %02X", default_address);
+    ret = atcab_init(&cfg_ateccx08a_i2c_default);
+
+    #endif
+
+    assert(!ret);
+
+    ESP_LOGI(TAG, "Successfully initialized ECC608");
 }
 
-/*
- * Start here
- */
 void app_main()
 {
-	ATCAIfaceCfg cfg = atca_cfg_init();	
-	i2c_init();
-	check_ret(atcab_init(&cfg));
-	printf("Your cryptochip is successfully connected!\n\n");
-	run();
+	initialize_ecc608();
+
+	uint8_t pubkey[64];
+	char csr[512];
+	size_t csr_size = sizeof(csr);
+
+	// Lock the config and data zones. This is one-time only.
+	atcab_lock_config_zone();
+	atcab_lock_data_zone();
+
+	// Generate a private key in slot 0
+	assert(!atcab_genkey(0, pubkey));
+
+	// Generate a CSR from the private key and CSR template.
+	assert(!atcacert_create_csr_pem(&csr_def, csr, &csr_size));
+
+	// Print public key and CSR
+	print_pubkey(pubkey, sizeof(pubkey));
+	print_csr(csr, csr_size);
+
+	printf("Please save this CSR to a file.\n");
 }
